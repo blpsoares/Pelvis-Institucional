ee44b6d54dd5f2c24b80c6ecc34de8b229c3079a
bryaanls <bryanluccas@hotmail.com>
2023-12-23 16:41:41 -0300

    test two main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -6,14 +6,14 @@ import "./index.css";
 const App = React.lazy(() => import("./App.jsx"));
 
 import { BrowserRouter } from "react-router-dom";
-import Loader from "./components/loader/index.jsx";
+// import Loader from "./components/loader/index.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
+    {/* <React.Suspense fallback={<Loader />}> */}
     <BrowserRouter>
-      <React.Suspense fallback={<Loader />}>
-        <App />
-      </React.Suspense>
+      <App />
     </BrowserRouter>
+    {/* </React.Suspense> */}
   </React.StrictMode>
 );
